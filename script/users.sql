f9c48a818ec15aa9e992e42e63d2fd28f0fa2d26
QurbanSharif <qurbanch48@gmail.com>
2022-12-23 10:40:45 +0500

    first commit

---
+++ script/users.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `username` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL )
